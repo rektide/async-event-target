ccf552593f71bea7b122bb56a5a5246e9f11b6e8
rektide de la faye <rektide@voodoowarez.com>
2019-07-29 23:10:35 -0400

    Refactor to leverage mixins.

--- async-event-target.js
+++ async-event-target.js
@@ -1,21 +1,38 @@
 "use module"
 import { EventTarget} from "yaeti"
+import { ExtendableEventMixin} from "./extendable-event.js"
 
 const __dispatchEvent= EventTarget.prototype.dispatchEvent
 
 class ExtendableEventTarget extends EventTarget{
-	dispatchEvent( evt){
-		var awaiting= []
-		function waitUntil( promise){
-			awaiting.push( promise)
+
+	static mixin( eventTarget){
+		if( !( eventTarget instanceof ExtendableEventTarget)&& eventTarget.dispatchEvent!== ExtendableEventTarget.prototype.dispatchEvent){
+			Object.defineProperties( eventTarget, {
+				dispatchEvent: {
+					value: ExtendableEventTarget.prototype.dispatchEvent,
+					configurable: true
+				}
+			})
 		}
-		// https://www.w3.org/TR/service-workers-1/#dom-extendableevent-waituntil
-		evt.waitUntil= waitUntil
+		return eventTarget
+	}
+
+	dispatchEvent( evt){
+		// enhance event
+		ExtendableEventMixin( evt)
+
+		// call underlying
 		var value= __dispatchEvent.call( this, evt)
 		if( value=== false){
 			return false
 		}
-		return new Promise( res=> setImmediate(()=> res( Promise.all( awaiting))))
+		// return a promise for when all the waits are done
+		return evt.then()
 	}
 }
-export default ExtendableEventTarget
+export const ExtendableEventTargetMixin= ExtendableEventTarget.mixin
+export {
+	ExtendableEventTarget as default,
+	ExtendableEventTargetMixin as mixin
+}
--- extendable-event.js
+++ extendable-event.js
@@ -5,13 +5,39 @@ export const
   extendableEventWaits= Symbol.for("ExtendableEvent::waits")
 
 export class ExtendableEvent extends Event{
+
+	static mixin( evt){
+		if( !evt.waitUntil){
+			if( evt.then){
+				throw new Error("Event already had a then")
+			}
+			// enhance!
+			Object.defineProperties( evt, {
+				waitUntil: {
+					value: ExtendableEvent.prototype.waitUntil,
+					configurable: true
+				},
+				then: {
+					value: ExtendableEvent.prototype.then,
+					configurable: true
+				}
+			})
+		}
+		return evt
+	}
+
+	// https://www.w3.org/TR/service-workers-1/#dom-extendableevent-waituntil
 	waitUntil( p){
 		let waits= this[ extendableEventWaits]|| (this[ extendableEventWaits]= [])
 		waits.push( p)
 	}
-	then(){
-		// all-in-all would be neat here!
-		return Promise.all( this[ extendableEventWait]|| [])
+	then( a, b){
+		// all-in-all would be neat here! so new waitUntils continue to work
+		return Promise.all( this[ extendableEventWait]|| []).then( a, b)
 	}
 }
-export default ExtendableEvent
+export const ExtendableEventMixin= ExtendableEvent.mixin
+export {
+	ExtendableEvent as default,
+	ExtendableEventMixin as mixin
+}
